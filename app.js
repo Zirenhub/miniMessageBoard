f80e097805644ba1144964eca0c7ea59747a2479
Erdinch <erdinchorhan02@gmail.com>
2022-12-03 15:55:47 +0200

    can send messages

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
+
+const main = require('./routes/index');
+app.use('/', main);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Form' });
+});
+
+router.post('/new', (req, res) => {
+  const name = req.body.name;
+  const message = req.body.message;
+
+  messages.push({ text: message, user: name, added: new Date() });
+
+  res.redirect('/');
 });
 
 module.exports = router;
